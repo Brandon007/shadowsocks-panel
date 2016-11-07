c44f5a82dd4ca6a62ff2cbe339cb39e047062c1c
Brandon007 <136836246@qq.com>
2016-11-07 15:19:21 +0800

    ..
    .

--- Migrations/20161107151652_create_table_wx_order.php
+++ Migrations/20161107151652_create_table_wx_order.php
@@ -8,11 +8,11 @@ class CreateTableWxOrder extends AbstractMigration
     public function change()
     {
         $this->table("wxorder")
-            ->addColumn('openid', 'integer', ['limit' => 32 'null' => false])
+            ->addColumn('openid', 'integer', ['limit' => 32,'null' => false])
             ->addColumn('money', 'integer', ['limit' => 5, 'comment'=> '单位:分'])
             ->addColumn('pay_time', 'integer', ['null' => false])
             ->addColumn('type', 'integer', ['limit'=> MysqlAdapter::INT_TINY, 'default'=> 0, 'comment'=> '类型 0-周卡 1-月卡 2-季卡 3-年卡'])
-            ->addColumn('status', 'integer', ['limit'=> MysqlAdapter::INT_TINY, 'default'=> 0, 'comment'=> '状态 0-未支付 1-已支付'])
+            ->addColumn('status', 'integer', ['limit'=> MysqlAdapter::INT_TINY, 'default'=> 0, 'comment'=> '状态 0-未支付 1-已支付','null' => false])
             ->addColumn('remark', 'string', ['limit' => 255])
             ->create();
     }
