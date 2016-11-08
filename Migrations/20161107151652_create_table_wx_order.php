dfd90bb564f08e574cf9c79399cc0d1b66a2b9dc
Brandon007 <136836246@qq.com>
2016-11-08 15:18:46 +0800

    change openid to string type
    ..

--- Migrations/20161107151652_create_table_wx_order.php
+++ Migrations/20161107151652_create_table_wx_order.php
@@ -9,7 +9,7 @@ class CreateTableWxOrder extends AbstractMigration
     {
         $this->table("wxorder")
             ->addColumn('out_trade_no', 'string', ['limit' => 28,'null' => false])
-            ->addColumn('openid', 'integer', ['limit' => 32,'null' => false])
+            ->addColumn('openid', 'string', ['limit' => 32,'null' => false])
             ->addColumn('money', 'integer', ['limit' => 5, 'comment'=> '单位:分'])
             ->addColumn('pay_time', 'integer', ['null' => false])
             ->addColumn('type', 'integer', ['limit'=> MysqlAdapter::INT_TINY, 'default'=> 0, 'comment'=> '类型 0-周卡 1-月卡 2-季卡 3-年卡'])
