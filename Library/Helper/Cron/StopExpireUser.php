b85648bdbc0e470138d0c79ece61082156a9e28b
Brandon007 <136836246@qq.com>
2016-11-12 18:20:34 +0800

    add cron to stop wechat user
    ..

--- Library/Helper/Cron/StopExpireUser.php
+++ Library/Helper/Cron/StopExpireUser.php
@@ -6,7 +6,8 @@
  */
 
 namespace Helper\Cron;
-
+include_once  '/home/wwwroot/shadowsocks-panel/Package/autoload.php'; // 引入 composer 入口文件
+include_once  '/home/wwwroot/wukongss/cfg.php';
 use Contactable\ICron;
 
 use Helper\Logger;
@@ -25,10 +26,24 @@ use Model\User;
 class StopExpireUser implements ICron
 {
     const STEP = 300; // 5分钟执行一次
+    private $options = [
+        'debug'  => true,
+        'app_id' => $cfg["appid"],
+        'secret' => $cfg["secret"],
+        'token'  => $cfg["token"],
+        // 'aes_key' => null, // 可选
+        'log' => [
+            'level' => 'debug',
+            'file'  => '/home/wwwlogs/wechat.log', // XXX: 绝对路径！！！！
+        ]
+        //...
+    ];
 
+    private $app = new Application($options);
     public function run()
     {
         $users = User::getUserArrayByExpire();
+        $wechatUsers = User::getWechatUserArrayByExpire();
         $notificationMail = Option::get('mail_stop_expire_notification');
         $mailContentTemplate = Option::get('custom_mail_stop_expire_content');
 
@@ -36,39 +51,59 @@ class StopExpireUser implements ICron
             Option::set('mail_stop_expire_notification', 0); // 设置邮件提醒的系统参数
         }
 
-        $mailer = Mailer::getInstance();
-        $mailer->toQueue(true);
+        // $mailer = Mailer::getInstance();
+        // $mailer->toQueue(true);
 
-        foreach ($users as $user) {
-            $user->stop();
-            Logger::getInstance()->info('user ['.$user->email.'] 未续费或流量超用已被暂停服务');
-            if ($notificationMail) {
-                $mail = new Mail();
-                $mail->to = $user->email;
-                $mail->subject = '[' . SITE_NAME . '] ' . "用户 {$user->nickname}，您的账户由于到期或流量超用已被暂停服务";
-                $params = [
-                    'nickname' => $user->nickname,
-                    'email' => $user->email,
-                    'useTraffic' => Utils::flowAutoShow($user->flow_up + $user->flow_down),
-                    'transfer' => Utils::flowAutoShow($user->transfer),
-                    'expireTime' => date('Y-m-d H:i:s', $user->expireTime)
-                ];
-                $mailContent = Utils::placeholderReplace($mailContentTemplate, $params);
-                $mailContent .= "<p style=\"padding: 1.5em 1em 0; color: #999; font-size: 12px;\">—— 本邮件由 ". SITE_NAME ." (<a href=\"".BASE_URL."\">".BASE_URL."</a>) 账户管控系统发送</p>";
-                $mail->content = $mailContent;
-                $mailer->send($mail);
-            }
-        }
+        // foreach ($users as $user) {
+        //     $user->stop();
+        //     Logger::getInstance()->info('user ['.$user->email.'] 未续费或流量超用已被暂停服务');
+        //     if ($notificationMail) {
+        //         $mail = new Mail();
+        //         $mail->to = $user->email;
+        //         $mail->subject = '[' . SITE_NAME . '] ' . "用户 {$user->nickname}，您的账户由于到期或流量超用已被暂停服务";
+        //         $params = [
+        //             'nickname' => $user->nickname,
+        //             'email' => $user->email,
+        //             'useTraffic' => Utils::flowAutoShow($user->flow_up + $user->flow_down),
+        //             'transfer' => Utils::flowAutoShow($user->transfer),
+        //             'expireTime' => date('Y-m-d H:i:s', $user->expireTime)
+        //         ];
+        //         $mailContent = Utils::placeholderReplace($mailContentTemplate, $params);
+        //         $mailContent .= "<p style=\"padding: 1.5em 1em 0; color: #999; font-size: 12px;\">—— 本邮件由 ". SITE_NAME ." (<a href=\"".BASE_URL."\">".BASE_URL."</a>) 账户管控系统发送</p>";
+        //         $mail->content = $mailContent;
+        //         $mailer->send($mail);
+        //     }
+        // }
         // 避免频繁更新 Option 单例对象，循环结束后再执行
         if ($notificationMail) {
             Option::set('mail_queue', 1);
         }
         // 2016-04-26 15:00 - by @Sendya Fixed issue #62
         // User::enableUsersByExpireTime(); // 启用已续费且流量未超过的用户
+
+
+        foreach ($wechatUsers as $wechatUser) {
+            sendTemplateMsg($app,$wechatUser->openid);
+            $wechatUser->stop();
+        }
     }
 
     public function getStep()
     {
         return time() + self::STEP;
+    }
+
+    public function sendTemplateMsg($app,$openid){
+        $userId = $openid;
+        $templateId = 'OtmnvVvHqBH9eUCC5-KkXV-QPVNDWkgpArvOlUbco04';
+        $url = 'http://wx.wukongss.com/bind.php';
+        $data = array(
+            "first"    => array("加速服务到期！", '#555555'),
+            "keyword1" => array("高级套餐", "#336699"),
+            "keyword2" => array("2014-10-10 ", "#FF0000"),
+            "remark"   => array("欢迎再次购买！", "#5599FF"),
+        );
+        $result = $app->notice->uses($templateId)->withUrl($url)->andData($data)->andReceiver($userId)->send();
+        var_dump($result);        
     }
 }
