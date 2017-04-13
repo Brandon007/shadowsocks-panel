a180091e261cfd5f8ccba6ba3f14b8bdf1bc23d8
Brandon007 <136836246@qq.com>
2017-04-13 17:37:54 +0800

    ..
    ..

--- Library/Controller/Api.php
+++ Library/Controller/Api.php
@@ -159,7 +159,6 @@ class Api
         $port = $_POST['port'];
         $timestamp = $_POST['timestamp'];
         $token = $_POST['token'];
-        $token = str_replace(' ', '+', $token);////替换空格为加号
         $sign = $_POST['sign'];
         if ($this->securityProcess($timestamp,$token,$sign)) {//通过api安全检验
             $nodes = null;
@@ -246,18 +245,18 @@ class Api
     * @param $sign 用户签名
     */
     protected function securityProcess($timestamp,$token,$sign){
-        Logger::getInstance()->info('before sign urlencode :' . $sign);
+        Logger::getInstance()->info('before sign urldecode :' . $sign);
         $sign = urldecode($sign);
-        Logger::getInstance()->info('after sign urlencode :' . $sign);
+        Logger::getInstance()->info('after sign urldecode :' . $sign);
         if (empty($timestamp) || empty($token) || empty($sign)) {//missing param
             throw new Error("param missing", 7001);
         }
         if (abs(time() - $timestamp) >600) {
             throw new Error("invilid timestamp", 7002);
         }
-        Logger::getInstance()->info('before token urlencode :' . $token);
+        Logger::getInstance()->info('before token urldecode :' . $token);
         $token = urldecode($token);
-        Logger::getInstance()->info('after token urlencode :' . $token);
+        Logger::getInstance()->info('after token urldecode :' . $token);
         $decryptedToken = Utils::decrypt($token,ENCRYPT_API_KEY);
         $port = explode('_', $decryptedToken)[1];
         Logger::getInstance()->info('decryptedToken:' . $decryptedToken);
